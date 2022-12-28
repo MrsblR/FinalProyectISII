bcfb5047613942e40a918e612750c23cb6942c8b
Mrsbl <mruelasq@unsa.edu.pe>
2022-12-28 15:55:43 -0500

    Refactoring

--- Application/src/config/dbconnections.js
+++ Application/src/config/dbconnections.js
@@ -1,5 +1,11 @@
+require("dotenv").config();
+const mysql = require("mysql2");
+// create the pool
+const pool = mysql.createPool({
+  host: process.env.HOST,
+  user: process.env.USER,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+});
 
-const mysql = require('mysql2');
-    // create the pool
-const pool = mysql.createPool({host:'localhost', user: 'root', database: 'attendance', password:'buscandoelcambio-5'});
 module.exports = pool;
--- Application/src/libs/tools.js
+++ Application/src/libs/tools.js
@@ -1,11 +1,9 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
 module.exports.toHash = (plaintext) => {
-    const salt = bcrypt.genSaltSync(10);
-    return bcrypt.hashSync(plaintext, salt);
-
-}
+  const salt = bcrypt.genSaltSync(10);
+  return bcrypt.hashSync(plaintext, salt);
+};
 module.exports.compareHash = (plaintext, hashtext) => {
-    return bcrypt.compareSync(plaintext, hashtext);
-
-}
+  return bcrypt.compareSync(plaintext, hashtext);
+};
